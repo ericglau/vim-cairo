59c6df3ed2c861809cb382bb27bfac75af87835b
Eric Lau <ericglau@outlook.com>
2021-10-13 21:43:34 -0400

    Initial commit

---
+++ .vimrc
@@ -0,0 +1,31 @@
+" Specify a directory for plugins
+" - For Neovim: stdpath('data') . '/plugged'
+" - Avoid using standard Vim directory names like 'plugin'
+call plug#begin('~/.vim/plugged')
+
+" Make sure you use single quotes
+
+Plug 'autozimu/LanguageClient-neovim', {
+    \ 'branch': 'next',
+    \ 'do': 'bash install.sh',
+    \ }
+
+" (Optional) Multi-entry selection UI.
+Plug 'junegunn/fzf'
+
+" Initialize plugin system
+call plug#end()
+
+au BufReadPost *.cairo set filetype=cairo
+au Filetype cairo set syntax=cairo
+
+let g:LanguageClient_serverCommands = {
+\ 'cairo': ['node', '~/.vim/cairo-ls/server/out/server.js', '--stdio']
+                        \ }
+
+let g:LanguageClient_loggingLevel = 'DEBUG'
+let g:LanguageClient_loggingFile =  expand('~/.vim/cairo-ls/cairo-language-client.log')
+let g:LanguageClient_serverStderr = expand('~/.vim/cairo-ls/cairo-language-server.log')
+
+nnoremap <silent> <F4> :call LanguageClient#textDocument_hover()<CR>
+nnoremap <silent> <F3> :call LanguageClient#textDocument_codeAction()<CR>
---
+++ install-vim-cairo.sh
@@ -0,0 +1,17 @@
+if [ ! -f ~/.vim/autoload/plug.vim ]; then curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim; else echo "plug.vim already exists - skipping"; fi
+if [ ! -f ~/.vim/syntax/cairo.vim ]; then curl -fLo ~/.vim/syntax/cairo.vim --create-dirs https://raw.githubusercontent.com/starkware-libs/cairo-lang/master/src/starkware/cairo/lang/ide/vim/syntax/cairo.vim; else echo "cairo.vim already exists - skipping"; fi
+if [ ! -f ~/.vim/cairo-ls/cairo-ls-0.0.4.vsix ]; then curl -fLo ~/.vim/cairo-ls/cairo-ls-0.0.4.vsix --create-dirs https://github.com/ericglau/cairo-ls/releases/download/v0.0.4/cairo-ls-0.0.4.vsix; else echo "cairo-ls-0.0.4.vsix already exists - skipping"; fi
+rm -rf vscode-cairo
+unzip -d vscode-cairo ~/.vim/cairo-ls/cairo-ls-0.0.4.vsix
+mkdir ~/.vim/cairo-ls
+cp -R vscode-cairo/extension/server ~/.vim/cairo-ls
+rm -rf vscode-cairo
+cp -n .vimrc ~/.vimrc
+retVal=$?
+vim +PlugInstall +qall
+if [ $retVal -ne 0 ]; then
+    echo "The ~/.vimrc file already exists and will not be overwritten. Manually edit its configuration according to https://github.com/ericglau/vim-cairo/blob/master/.vimrc"
+    echo "Vim Cairo LS has been installed, but the ~/.vimrc file must be manually configured."
+else
+    echo "Vim Cairo LS has been successfully installed!"
+fi
